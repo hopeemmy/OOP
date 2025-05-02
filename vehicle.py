aa67aadba2ec64958dd5fa120073463b02fd01fd
hopeemmy <hoppyclassic25@gmail.com>
2025-05-02 11:26:27 -0600

    Todo for the OOP

---
+++ vehicle.py
@@ -0,0 +1,27 @@
+"""
+    Name: vehicle.py
+    Author: Hope Ogbonda
+    Created: 5/2/25
+    Purpose: OOP that models a vehicle
+"""
+
+# 1.  Define a class named vehicle.
+
+# 2. Add and __init__ method with four attributes:
+    #name (string)
+    # - model_year (int)
+    #- speed (float)
+    #- is_engine_on (bool)
+
+
+#3. Define four methods:
+    # - start_engine: sets is_engine_on to True
+    # - stop_engine: sets is_engine_on to False
+    #- accelerate: increases speed by a user-defined value
+    #- brake: sets speed to 0
+
+#4. Use input() to collect vehicle name and model year from user.
+
+#5. Create an instance of the Vehicle class using the user inputs.
+
+#6. Use the methods and print the updated vehicle state after each action.
