ef899ea208c3aa568c432ad6a3bd29c3b83a15b9
hopeemmy <hoppyclassic25@gmail.com>
2025-05-02 18:39:26 -0600

    working on sounds
    Still updating code to work properly

--- vehicle.py
+++ vehicle.py
@@ -5,13 +5,25 @@
     Purpose: OOP that models a vehicle
 """
 
+import pygame
+import time
+
+# Initializing the mixer
+pygame.mixer.init()
 
 # Print title banner 
 print("=" * 50)
 print("🚗 The Intelligent Ride".center(50))
 print("A Python OOP Simulation of a Smart Vehicle".center(50))
-print("=" * 50) #+ '\n' )
+print("=" * 50)
 print()
+
+def play_sound(car_sounds):
+    pygame.mixer.music.load(car_sounds)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        time.sleep(0.1)
+    
 
 # Define a class named vehicle
 class Vehicle:
@@ -23,10 +35,15 @@ class Vehicle:
         self.is_engine_on = False       # Engine status starts as off
         
 
+    def open_door(self):
+        print("Door opened. Welcome in!")
+        play_sound("door_open.mp3")
+
     # Method to start the engine
     def start_engine(self):
         self.is_engine_on = True                 # Turn the engine on
         print(f"{self.name}'s engine started")    # Inform the user
+        play_sound("engine_start.mp3")
 
     # Method to stop the engine
     def stop_engine(self):                         
@@ -39,6 +56,7 @@ class Vehicle:
         if self.is_engine_on:
             self.speed += speed_increase
             print(f"{self.name} accelerated to {self.speed} mph.")  # Show new speed
+            play_sound("accelerate.mp3")
         else:
             print("Engine is off. Please start the engine first.")
 
@@ -48,19 +66,56 @@ class Vehicle:
         self.speed = 0
         print(f"{self.name} has stopped.")
 
+    
+    # Exiting the vehicle
+    def exit_vehicle(self):
+        print(" You stepped out of the car")
+        time.sleep(1)
+        print("Door closed.")
+        play_sound("door_open.mp3")
 
-#4. Collect user input for vehicle name and model year
+#--------------------- MAAIN PROGRAM --------------------------------#
+# Collect user input for vehicle name and model year
 vehicle_name = input("Enter the vehicle name: ")
 vehicle_year = int(input("Enter the model year: "))
 
 # Create an instance of the Vehicle class using the user inputs.
 my_vehicle = Vehicle(vehicle_name, vehicle_year)
 
-#Use the methods and print the updated vehicle state after each action.
+
+
+#Use the methods and print the updated vehicle state after each action
+my_vehicle.open_door()
+time.sleep(1)
+
 my_vehicle.start_engine()       # Start the engine
-my_vehicle.accelerate(30)       # Accelerate by 30 mph
-my_vehicle.brake()              # Apply brakes
-my_vehicle.stop_engine()        # Stop the engine
+
+# Show menu options after engine starts
+while my_vehicle.is_engine_on:
+    print("\nChoose an action")
+    print("1. Drive")
+    print("2. Brake")
+    print("3. Stop Engine and Exit")
+    user_choice = input("Enter selection (1/2/3): ")
+
+    if user_choice =="1":
+        increase = int(input("Enter speed to drive (mph): "))
+        my_vehicle.accelerate(increase)
+        
+    elif user_choice == "2":
+        my_vehicle.brake()
+
+    elif user_choice == "3":
+        my_vehicle.stop_engine()
+        my_vehicle.exit_vehicle()
+        break
+
+    else:
+        print("Invalid inout. Try again.")
+
+        #my_vehicle.accelerate(30)       # Accelerate by 30 mph
+        #my_vehicle.brake()              # Apply brakes
+        #my_vehicle.stop_engine()        # Stop the engine
 
 
 # Add sound to the engine started
